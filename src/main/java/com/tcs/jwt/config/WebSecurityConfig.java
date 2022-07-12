833b861292446d94c99c4ef552d576f778aa2783
Pedro Martin del Campo Gonzalez <pedro.martindelcampo.gonzalez@gmail.com>
2022-07-12 13:55:01 -0700

    Configure AuthenticationManager

--- src/main/java/com/tcs/jwt/config/WebSecurityConfig.java
+++ src/main/java/com/tcs/jwt/config/WebSecurityConfig.java
@@ -1,15 +1,27 @@
 package com.tcs.jwt.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  private UserDetailsService userDetailsService;
+
+  public WebSecurityConfig(
+    @Autowired UserDetailsService userDetailsService
+  ) {
+    this.userDetailsService = userDetailsService;
+  }
 
   @Bean
   public PasswordEncoder passwordEncoder() {
@@ -27,6 +39,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
       .csrf().disable()
       .sessionManagement()
         .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+  }
+
+  @Override
+  public void configure(AuthenticationManagerBuilder builder) throws Exception {
+    builder
+      .userDetailsService(userDetailsService)
+      .passwordEncoder(passwordEncoder());
+  }
+
+  @Bean
+  @Override
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
   }
 
 }
